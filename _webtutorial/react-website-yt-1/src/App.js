470c24fb1a0c137f820d51bc5221dc02e23d1e78
Lendus Casey <lendus.casey@gmail.com>
2022-07-10 18:56:10 +0000

    finished webtutorial

--- _webtutorial/react-website-yt-1/src/App.js
+++ _webtutorial/react-website-yt-1/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar.js';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home.js';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+          <Route path='/' element = {<Home />}/>
+          <Route path='/services' element ={<Services/>} />
+          <Route path='/products' element ={<Products/>} />
+          <Route path='/sign-up' element ={<SignUp/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
